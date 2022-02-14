0ab29a4c0d053b5e22c90649cec7b433cc2d4bef
MandL27 <mandl27g@gmail.com>
2022-02-14 13:29:38 -0500

    Implement level draft and Coil sprites

--- Entities/Player/Player.cs
+++ Entities/Player/Player.cs
@@ -4,6 +4,7 @@ using System;
 public class Player : Entity
 {
 	// Declare member variables here.
+	AnimatedSprite PlayerSprite;
 	Vector2 InitialPos = Vector2.Zero;
 	Vector2 Direction = Vector2.Zero;
 	Vector2 Heading = Vector2.Up;
@@ -15,7 +16,8 @@ public class Player : Entity
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		
+		base._Ready();
+		PlayerSprite = GetNode<AnimatedSprite>("PlayerSprite");
 	}
 
 	// Called every tick. 'delta' is the elapsed time since the previous frame.
@@ -55,19 +57,29 @@ public class Player : Entity
 				BufferReady = false;
 				Buffer = Vector2.Zero;
 				InitialPos = Position;
-				// TODO: collision
 				Direction = Direction.Normalized();
 				Heading = Direction.Normalized();
-				Action = Action.Step;
-				Frames = 10;
+				if (!IsAdjacentTileSolid(Direction))
+				{
+					Action = Action.Step;
+					Frames = 10;
+				}
 			}
 			else if (Input.IsActionPressed("jump"))
 			{
 				InitialPos = Position;
-				// TODO: collision
-				Direction = Heading;
-				Action = Action.LongJump;
-				Frames = 32;
+				if (!IsAdjacentTileSolid(Heading * 2))
+				{
+					Direction = Heading;
+					Action = Action.LongJump;
+					Frames = 32;
+				}
+				else if (!IsAdjacentTileSolid(Heading))
+				{
+					Direction = Heading / 2;
+					Action = Action.LongJump;
+					Frames = 32;
+				}
 			}
 			else if (Input.GetActionStrength("turn_left") > Input.GetActionStrength("turn_right"))
 			{
@@ -110,15 +122,36 @@ public class Player : Entity
 				}
 				break;
 		}
+		switch ((int)Mathf.Rad2Deg(Heading.Angle()))
+		{
+			case 0:
+				PlayerSprite.Animation = "RightIdle";
+				break;
+			case -90:
+				PlayerSprite.Animation = "UpIdle";
+				break;
+			case 180:
+			case -180:
+				PlayerSprite.Animation = "LeftIdle";
+				break;
+			case 90:
+				PlayerSprite.Animation = "DownIdle";
+				break;
+		}
 	}
-	
+
+	private bool IsAdjacentTileSolid(Vector2 direction)
+	{
+		return GetWorld2d().DirectSpaceState.IntersectRay(GlobalPosition, GlobalPosition + (direction * 72), null, 1, false, true).Count > 0;
+	}
+
 	private void OnBodyEntered(object area)
 	{
 		Area2D a = (Area2D)area;
-		switch(a.CollisionLayer)
+		switch (a.CollisionLayer)
 		{
 			case 8: // enemy
-				// TODO: kill player
+					// TODO: kill player
 				GD.Print("Enemy touched");
 				break;
 			case 16: // enemy line of sight
@@ -126,7 +159,7 @@ public class Player : Entity
 				GD.Print($"Spotted by {a.GetParent().GetParent().Name}");
 				break;
 			case 32: // pickup
-				// TODO: score, etc.
+					 // TODO: score, etc.
 				break;
 		}
 	}
